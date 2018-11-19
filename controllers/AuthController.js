054cd0e0674e3a202d6780fef2c7ca875030df5e
Jose Segura <00001617@uca.edu.sv>
2018-11-19 12:48:47 -0600

    login y registro funcionando, arreglado el package.json, la ruta index arreglada y modelo user arreglado

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-=======
-'use strict';
->>>>>>> ff39147161a3fb1ad70030446708ee63f949f798
+
 var mongoose = require("mongoose");
 var passport = require("passport");
 var User = require("../models/User");
 
-<<<<<<< HEAD
 var userController = {};
 
 // Restrict access to root page
@@ -30,36 +26,10 @@ userController.doRegister = function(req, res) {
     passport.authenticate('local')(req, res, function () {
       console.log('iniciada sesion...supuestamente');
       res.redirect('/');
-=======
-var authController = {};
-
-// Restrict access to root page
-authController.home = function (req, res) {
-  res.render('index', {
-    user: req.user,
-    title: 'DLCloud'
-  });
-};
-// Post registration
-authController.doRegister = function (req, res) {
-  User.register(new User({
-    username: req.body.username,
-    name: req.body.name
-  }), req.body.password, function (err, user) {
-    if (err) {
-      return res.render('/', {
-        user: req.body.username
-      });
-    }
-
-    passport.authenticate('local')(req, res, function () {
-      res.redirect('/profile');
->>>>>>> ff39147161a3fb1ad70030446708ee63f949f798
     });
   });
 };
 
-<<<<<<< HEAD
 // Go to login page
 userController.login = function(req, res) {
   res.render('login');
@@ -69,32 +39,13 @@ userController.login = function(req, res) {
 userController.doLogin = function(req, res) {
   passport.authenticate('local')(req, res, function () {
     res.redirect('/');
-=======
-// Post login
-authController.doLogin = function (req, res) {
-  passport.authenticate('local', {
-    failureRedirect: '/',
-    failureFlash: true
-  })(req, res, function () {
-    res.redirect('/profile');
->>>>>>> ff39147161a3fb1ad70030446708ee63f949f798
   });
 };
 
 // logout
-<<<<<<< HEAD
 userController.logout = function(req, res) {
   req.logout();
   res.redirect('/');
 };
 
 module.exports = userController;
-=======
-authController.logout = function (req, res) {
-  req.logout();
-  res.clearCookie('sessionid', {path: '/'});
-  res.redirect('/');
-};
-
-module.exports = authController;
->>>>>>> ff39147161a3fb1ad70030446708ee63f949f798
--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
@@ -10,18 +9,4 @@ var UserSchema = new Schema({
 });
 
 UserSchema.plugin(passportLocalMongoose);
-=======
-'use strict';
-const mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    passportLocalMongoose = require('passport-local-mongoose');
-
-let UserSchema = new Schema({
-    username: String,
-    password: String
-});
-
-UserSchema.plugin(passportLocalMongoose);
-
->>>>>>> ff39147161a3fb1ad70030446708ee63f949f798
 module.exports = mongoose.models.user || mongoose.model('user', UserSchema);
